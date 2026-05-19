0f87645e4dc10e0d9488461945e944826d9cea5d
sankhan55 <sana.fatima@netopz.com>
2026-05-19 12:24:47 +0500

    migration set3

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,17 +13,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(TenantSeeder::class);
-        User::factory(25)->create();
+        // $this->call(TenantSeeder::class);
+        // User::factory(25)->create();
         $this->call(PermissionTableSeeder::class);
-        $this->call(ImportSQLSeeder::class);
-        $this->call(CreateAdminUserSeeder::class);
+        $user = User::create([
+            'name' => 'Team Cabinets',
+            'email' => 'super-user@demo.com',
+            'username' => 'Pinnacle Super User',
+            'password' => Hash::make('password'),
+            'is_super_user' => 1,
+            'country_id' => 233,
+            'state_id' => State::inRandomOrder()->first()->id,
+            'address' => '915 Doyle Road Suite 303 -225',
+            'city_name' => 'Deltona',
+            'county_name' => 'Volusia County',
+            'tenant_id' => Tenant::inRandomOrder()->first()->id,
+        ]);
+        // $this->call(ImportSQLSeeder::class);
+        // $this->call(CreateAdminUserSeeder::class);
         // $this->call(CountrySeeder::class);
         // $this->call(ProductCatalogSeeder::class);
         // $this->call(ProductSectionSeeder::class);
-        $this->call(ProductSeeder::class);
-        $this->call(TaxValueSeeder::class);
-        $this->call(DoorColorsSeeder::class);
+        // $this->call(ProductSeeder::class);
+        // $this->call(TaxValueSeeder::class);
+        // $this->call(DoorColorsSeeder::class);
 
     }
 }
